8de2d8a865c3ac454b98506349aefe1ee3fe7066
SKKUKang <jun02727@g.skku.edu>
2024-02-10 00:20:31 +0900

    권한 최소화

--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
-// (popup<->background)DOMContentLoaded 이벤트가 발생하면 백그라운드 페이지에서 변수를 가져오는 함수 호출
-
 document.addEventListener("DOMContentLoaded", function() {
     // Activate 버튼 클릭 시
     document.getElementById("activateButton").addEventListener("click", function() {
-        // popup->background 변수변경 알림
-        chrome.runtime.sendMessage("activate");
         //popup->content에 실행명령
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const tab = tabs[0];
@@ -23,8 +19,6 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Deactivate 버튼 클릭 시
     document.getElementById("deactivateButton").addEventListener("click", function() {
-        // popup->background 변수변경 알림
-        chrome.runtime.sendMessage("deactivate");
         //popup->content에 실행명령
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             const tab = tabs[0];
